7f8d47c6d11bbba9e55cee2dfb710155d0030bb9
BachVy <bachngocvy25112005@gmail.com>
2026-03-14 18:43:11 +0700

    Update add func for FAIL

--- src/calculator.py
+++ src/calculator.py
@@ -1,5 +1,9 @@
+# def add(a, b):
+#     return a + b
+
+#=============== Trường hợp push fail ===============
 def add(a, b):
-    return a + b
+    return a - b  # Sửa dấu + thành dấu -
 
 def subtract(a, b):
     return a - b
